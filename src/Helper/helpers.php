74657acb3c184b3eb6f9ebbc03da68c967df76fe
Shocker Li <alexanderliliang@163.com>
2016-06-13 16:26:10 +0800

    Add function `single` and `log`

--- src/Helper/helpers.php
+++ src/Helper/helpers.php
@@ -3,14 +3,56 @@
 use Upfor\App;
 
 if (!function_exists('app')) {
+
     /**
      * Get the specified app instance
      *
      * @param  string $name
      * @return \Upfor\App
      */
-    function app($name = 'default') {
-        return App::getInstance($name);
+    function app($name = null) {
+        static $currentApp = 'default';
+
+        if ($name) {
+            $currentApp = $name;
+        }
+
+        return App::getInstance($currentApp);
+    }
+
+}
+
+if (!function_exists('single')) {
+
+    /**
+     * Gets a single instance of a class
+     * 
+     * @param  string  $key   The value or object name
+     * @param  Closure        The closure that defines the object
+     * @return mixed
+     */
+    function single($key, $value = null) {
+        return app()->singleton($key, $value);
+    }
+
+}
+
+if (!function_exists('log')) {
+
+    /**
+     * Log a debug message to the logs.
+     *
+     * @param  mixed $level
+     * @param  mixed $message
+     * @param  array $context
+     * @return \Upfor\Exception\Logger|null
+     */
+    function log($level, $message, $context = []) {
+        if (is_null($message)) {
+            return single('logger');
+        }
+
+        return single('logger')->log($level, $message, $context);
     }
 
 }
